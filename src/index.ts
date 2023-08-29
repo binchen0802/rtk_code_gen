540ef1744c51ae624b098c705f540198da04cb11
Bin.Chen <bin.chen@chargelab.co>
2023-08-29 13:31:10 -0600

    add log file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
-import path from 'path';
-import fs from 'fs';
-import type { CommonOptions, ConfigFile, GenerationOptions, OutputFileOptions } from './types';
-import { isValidUrl, prettify } from './utils';
-export type { ConfigFile } from './types';
+import path from "path";
+import fs from "fs";
+import type {
+  CommonOptions,
+  ConfigFile,
+  GenerationOptions,
+  OutputFileOptions,
+} from "./types";
+import { isValidUrl, prettify } from "./utils";
+export type { ConfigFile } from "./types";
 
-export async function generateEndpoints(options: GenerationOptions): Promise<string | void> {
+export async function generateEndpoints(
+  options: GenerationOptions
+): Promise<string | void> {
   const schemaLocation = options.schemaFile;
 
   const schemaAbsPath = isValidUrl(options.schemaFile)
@@ -12,12 +19,16 @@ export async function generateEndpoints(options: GenerationOptions): Promise<str
     : path.resolve(process.cwd(), schemaLocation);
 
   const sourceCode = await enforceOazapftsTsVersion(() => {
-    const { generateApi } = require('./generate');
+    const { generateApi } = require("./generate");
     return generateApi(schemaAbsPath, options);
   });
   const outputFile = options.outputFile;
   if (outputFile) {
-    fs.writeFileSync(path.resolve(process.cwd(), outputFile), await prettify(outputFile, sourceCode));
+    console.log("outputFile:", outputFile);
+    fs.writeFileSync(
+      path.resolve(process.cwd(), outputFile),
+      await prettify(outputFile, sourceCode)
+    );
   } else {
     return await prettify(null, sourceCode);
   }
@@ -26,7 +37,7 @@ export async function generateEndpoints(options: GenerationOptions): Promise<str
 export function parseConfig(fullConfig: ConfigFile) {
   const outFiles: (CommonOptions & OutputFileOptions)[] = [];
 
-  if ('outputFiles' in fullConfig) {
+  if ("outputFiles" in fullConfig) {
     const { outputFiles, ...commonConfig } = fullConfig;
     for (const [outputFile, specificConfig] of Object.entries(outputFiles)) {
       outFiles.push({
@@ -46,8 +57,10 @@ export function parseConfig(fullConfig: ConfigFile) {
  * That should prevent enums from running out of sync if both libraries use different TS versions.
  */
 function enforceOazapftsTsVersion<T>(cb: () => T): T {
-  const ozTsPath = require.resolve('typescript', { paths: [require.resolve('@rtk-query/oazapfts-patched')] });
-  const tsPath = require.resolve('typescript');
+  const ozTsPath = require.resolve("typescript", {
+    paths: [require.resolve("@rtk-query/oazapfts-patched")],
+  });
+  const tsPath = require.resolve("typescript");
   const originalEntry = require.cache[ozTsPath];
   try {
     require.cache[ozTsPath] = require.cache[tsPath];
